72044329abb7321d52bad5e0c439460b55d5729b
anpplex <35098199+anpplex@users.noreply.github.com>
2026-05-14 10:48:20 +0800

    Add package install script

---
+++ source/install/doinst.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /boot/config/plugins/lucky
+mkdir -p /boot/config/plugins/lucky/luckyconf
+mkdir -p /usr/local/sbin
+
+if [ ! -f /boot/config/plugins/lucky/lucky.cfg ]; then
+  cp /usr/local/emhttp/plugins/lucky/defaults/lucky.cfg /boot/config/plugins/lucky/lucky.cfg
+fi
+
+chmod 0755 /usr/local/lucky/lucky
+ln -sf /usr/local/lucky/lucky /usr/local/sbin/lucky
+chmod 0755 /etc/rc.d/rc.lucky
+chmod 0755 /usr/local/emhttp/plugins/lucky/event/started
+chmod 0755 /usr/local/emhttp/plugins/lucky/scripts/lucky-status
